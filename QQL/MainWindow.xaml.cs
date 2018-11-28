446459e3dd12b517a234f69d92a04fbb31b647c1
Dream.Machine <944095635@qq.com>
2018-11-29 01:52:29 +0800

    2018-11-29

--- QQL/MainWindow.xaml.cs
+++ QQL/MainWindow.xaml.cs
@@ -1,25 +1,8 @@
-﻿using DMSkin.WPF.Small;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Input;
 
 namespace QQL
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : DMSkinWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
--- QQL/Properties/AssemblyInfo.cs
+++ QQL/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("QQL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DMSKIN")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DMSKIN")]
 [assembly: AssemblyProduct("QQL")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
